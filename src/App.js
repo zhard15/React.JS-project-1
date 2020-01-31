d11888791476677f7f0388f61ef7023ad82c08db
zhard15 <56136793+zhard15@users.noreply.github.com>
2020-01-31 07:59:03 -0600

    The onClick functions

--- src/App.js
+++ src/App.js
@@ -12,6 +12,60 @@ class App extends Component {
               result: ""
          }
       }
+    
+      onClick = button => {
+          
+          if(button === "=") {
+              this.calculate()
+          }
+          
+          else if(button === "C") {
+              this.reset()
+          }
+          
+          else if(button === "CE") {
+              this.backspace()
+          }
+          
+          else {
+              this.setState({
+                  result: this.state.result + button
+                  
+              })
+              
+          }
+      };
+             
+    
+        calculate = () => {
+            try {
+                this.setState({
+                    result: (eval(this.state.result) || "") + ""
+                    
+                })
+            } catch (e) {
+                this.setState({
+                    result: "error"
+                    
+                })
+                
+            }
+            
+        };
+
+        reset = ()  => {
+            this.setState({
+                result: ""
+                
+            })
+            
+        };
+        backspace = () => {
+            this.setState({
+                result: this.state.result.slice(0, -1)
+            })
+            
+        };
       
       render() {
           return (
